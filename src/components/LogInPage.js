caaf1f56db9dcfc1ae3c94cc30ce1ce27375fcdc
Lia Ximenes <ximeneslia@gmail.com>
2021-06-29 01:29:28 -0300

    feature/login - implementaçao da rota com axios

--- src/components/LogInPage.js
+++ src/components/LogInPage.js
@@ -5,27 +5,74 @@ import { useState, useContext, useEffect } from 'react';
 
 import gato from "../images/gato.png"
 
+import UserContext from '../context/UserContext';
+
 export default function Login(){
+    const {user, setUser} = useContext(UserContext);
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    let history = useHistory();
+
+    const body = {
+        email,
+        password
+    }
+
+    useEffect(() => {
+        if (localStorage.length !== 0){
+            const listString = localStorage.getItem("list");
+            const list = JSON.parse(listString);
+            setUser(list)
+            console.log(list)
+            //history.push("/main-page")
+        }
+    }, [])
+
+
+    function loginCustomer(){
+        const request = axios. post("http://localhost:4000/log-in", body);
+        request.then((response) => {console.log(response.data)
+            setUser(response.data);
+
+            const tokenString = JSON.stringify(user);
+            localStorage.setItem('list', tokenString);
+            console.log(localStorage.getItem('list'))   
+            //history.push("/main-page")
+        });
+        request.catch(errors)
+
+        function errors(error){
+            if(error.response.status === 401){
+                alert("email ou senha incorretos")
+            }
+
+            setEmail("");
+            setPassword("");
+        }
+    }
+
+
     return(
-        <>
-            <FrontPage>
-                <Photo>
-                    <img src={gato}/>
-                </Photo>
-                
+        <FrontPage>
+            <Photo>
+                <img src={gato}/>
+            </Photo>
+            
 
-                <h1>AnúbisStore</h1>
-                <h2>Everything your cat needs. By Kat Dee</h2>
+            <h1>AnúbisStore</h1>
+            <h2>Everything your cat needs. By Kat Dee</h2>
 
-                <input type="text" placeholder="Email"/>
-                <input type="password" placeholder="Senha"/>
+            <input type="texte" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)}/>
 
-                <button>Entrar</button>
-                <Link to="/sign-up">
-                    <p>Ainda não tem cadastro? Faça agora!</p>
-                </Link>
-            </FrontPage>
-        </>
+            <button onClick={loginCustomer}>Entrar</button>
+
+            <Link to="/sign-up">
+                <p>Ainda não tem cadastro? Faça agora!</p>
+            </Link>
+        </FrontPage>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@ import {useState} from "react";
 import UserContext from './context/UserContext';
 
 import Login from "./components/LogInPage.js"
+import Signup from "./components/SignUpPage.js"
 
 
 function App(){
-    //const [user, setUser] = useState(localStorage.length!==0 ? JSON.parse(localStorage.getItem('list')) : [])
+    const [user, setUser] = useState(localStorage.length!==0 ? JSON.parse(localStorage.getItem('list')) : [])
 
     return(
-        <UserContext.Provider >
+        <UserContext.Provider value={{user, setUser}}>
             <BrowserRouter> 
 			<Switch>
 				<Route path="/" exact>
                     <Login />
+				</Route>
+
+                <Route path="/sign-up" exact>
+                    <Signup />
 				</Route>
             
 			</Switch>
