068beb70bb4a5f23ce041991fa4a6e6060516516
Lia Ximenes <ximeneslia@gmail.com>
2021-07-01 21:43:32 -0300

    commit para merge

--- src/components/home/Products.js
+++ src/components/home/Products.js
@@ -4,11 +4,7 @@ export default function Products ({allProducts, setSelectedProducts}) {
     function addOnCart(id){
         //to-do
     };
-<<<<<<< HEAD
-    
-=======
     console.log(allProducts);
->>>>>>> main
     return (
         <Body>
             <List>
